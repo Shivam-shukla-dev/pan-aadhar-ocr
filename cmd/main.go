7b21aaa8895c1e1d2f19566f96f00aa284220d1e
Shivam-Shukla2 <Shivam.Shukla2@ibm.com>
2023-09-12 14:35:38 +0530

    Modifying code for AAdhar card fetching

--- cmd/main.go
+++ cmd/main.go
@@ -3,65 +3,73 @@ package main
 import (
 	"bytes"
 	"encoding/base64"
+	"fmt"
 	"image"
 	"image/jpeg"
+	"io/ioutil"
 	"log"
-	"net/http"
+	"regexp"
+	"strings"
 
 	"github.com/anthonynsimon/bild/effect"
 	"github.com/anthonynsimon/bild/segment"
-	"github.com/gofiber/fiber/v2"
 	"github.com/otiai10/gosseract/v2"
-	"github.com/tidwall/gjson"
 )
 
 func main() {
-	app := fiber.New()
-	app.Post("/ocr", func(c *fiber.Ctx) error {
-		request := string(c.Body())
-		if gjson.Valid(request) {
-			// Get base64 from json request
-			base64image := gjson.Get(request, "img").String()
+	// Get base64 from json request
+	byt, err := ioutil.ReadFile("/Users/shivamshukla/Downloads/aadh.png")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-			// Decode base64 to byte
-			sDec, err := base64.StdEncoding.DecodeString(base64image)
-			if err != nil {
-				log.Fatal(err)
-			}
+	var base64image string
 
-			// Decode byte to image struct
-			img, _, err := image.Decode(bytes.NewReader(sDec))
-			if err != nil {
-				log.Fatalln(err)
-			}
+	// Append the base64 encoded output
+	base64image = toBase64(byt)
+	// Decode base64 to byte
+	sDec, err := base64.StdEncoding.DecodeString(base64image)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-			// Convert Image to grayscale
-			grayscale := effect.Grayscale(img)
+	// Decode byte to image struct
+	img, _, err := image.Decode(bytes.NewReader(sDec))
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-			// Convert Image to threshold segment
-			threshold := segment.Threshold(grayscale, 128)
+	// Convert Image to grayscale
+	grayscale := effect.Grayscale(img)
 
-			// Convert Image to Bytes
-			buf := new(bytes.Buffer)
-			jpeg.Encode(buf, threshold, nil)
+	// Convert Image to threshold segment
+	threshold := segment.Threshold(grayscale, 128)
 
-			// Initiation Gosseract new client
-			client := gosseract.NewClient()
+	// Convert Image to Bytes
+	buf := new(bytes.Buffer)
+	jpeg.Encode(buf, threshold, nil)
 
-			// close client when the main function is finished running
-			defer client.Close()
+	// Initiation Gosseract new client
+	client := gosseract.NewClient()
 
-			// Read byte to image and set whitelist character
-			client.SetImageFromBytes(buf.Bytes())
-			client.SetWhitelist(" -:/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+	// close client when the main function is finished running
+	defer client.Close()
 
-			// Get text result from OCR
-			text, _ := client.Text()
+	// Read byte to image and set whitelist character
+	client.SetImageFromBytes(buf.Bytes())
+	client.SetWhitelist(" -:/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+	client.SetLanguage("eng", "hin")
+	// Get text result from OCR
+	text, _ := client.Text()
+	// return the response
+	text = strings.Replace(text, "\n", " ", -1)
+	pattern := `\d{4}\s\d{4}\s\d{4}`
+	re := regexp.MustCompile(pattern)
+	match := re.FindAllString(text, -1)
+	//fmt.Println(text)
+	fmt.Println(match[0])
+}
 
-			// return the response
-			return c.JSON(&fiber.Map{"status": "OK", "response": text})
-		}
-		return c.Status(http.StatusNotAcceptable).JSON(&fiber.Map{"status": "Request not JSON"})
-	})
-	log.Fatal(app.Listen(":8000"))
+func toBase64(b []byte) string {
+	return base64.StdEncoding.EncodeToString(b)
 }
--- go.mod
+++ go.mod
@@ -4,23 +4,5 @@ go 1.19
 
 require (
 	github.com/anthonynsimon/bild v0.13.0
-	github.com/gofiber/fiber/v2 v2.49.1
 	github.com/otiai10/gosseract/v2 v2.4.1
-	github.com/tidwall/gjson v1.16.0
-)
-
-require (
-	github.com/andybalholm/brotli v1.0.5 // indirect
-	github.com/google/uuid v1.3.1 // indirect
-	github.com/klauspost/compress v1.16.7 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
-	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
-	github.com/tidwall/match v1.1.1 // indirect
-	github.com/tidwall/pretty v1.2.0 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.49.0 // indirect
-	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/sys v0.11.0 // indirect
 )
